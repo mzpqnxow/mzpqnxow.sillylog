2fd388d304553230b782996b556277da0eaa77db
Adam Greene <copyright@mzpqnxow.com>
2020-02-24 13:22:42 -0500

    Initial checking of setup.py / setup.cfg

--- setup.py
+++ setup.py
@@ -23,10 +23,27 @@ NAME = '.'.join(NAMESPACE + [PROJECT_NAME])
 NAME = PROJECT_NAME
 ABOUT = {}
 
+# Use https://pypi.org/classifiers/ for reference
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Natural Language :: English',
+    'Topic :: Software Development :: Libraries']
+
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     name=NAME,
+    classifiers=CLASSIFIERS,
     packages=find_packages(),
     install_requires=REQUIRED,
     author=AUTHOR,
